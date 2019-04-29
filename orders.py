13b1c11d4533146bc9f2c7a53758db03dfa92901
Kirt Morgan <kirtmorgan@live.com>
2019-04-29 10:18:28 +0100

    Practiced error handling and opening files within python

---
+++ orders.py
@@ -0,0 +1,24 @@
+# Error handling/Opening files
+# This is the section on opening files
+try:
+    # The file gets stored in the variable
+    file = open('order.txt', 'r')
+
+    # Read line by line and store in another variable(list_of_lines)
+    list_of_lines = file.readlines()
+    print(list_of_lines)
+    # Iterate over the list_of_lines/Print out each line
+    for line in list_of_lines:
+        print(line.strip())
+
+    # Clean up by closing the file
+    file.close()
+
+# This is the section on error handling
+except FileNotFoundError as errmsg:
+    print('There has been a error, File not found')
+    print(errmsg)
+    raise
+
+
+
